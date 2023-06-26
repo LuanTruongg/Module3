32fd6aa9b330f0e7090d21a7d5af8cb875b8e53c
LuanTruongg <luantruong020302@gmail.com>
2023-06-26 14:01:07 +0700

    URL Generation and ambient values

--- Module3/Controllers/UrlGenerationController.cs
+++ Module3/Controllers/UrlGenerationController.cs
@@ -4,13 +4,14 @@ namespace Module3.Controllers
 {
     public class UrlGenerationController : Controller
     {
+        [HttpGet("custom")]
         public IActionResult Source()
         {
             // Generates /UrlGeneration/Destination
             var url = Url.Action("Destination");
             return Ok($" URL = {url}");
         }
-
+        [HttpGet("custom/url/to/destination")]
         public IActionResult Destination()
         {
             return Ok("Hello");
