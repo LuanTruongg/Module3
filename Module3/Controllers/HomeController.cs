d5ccb6dbe012eecae7d712a1869c78f15dd20ece
LuanTruongg <luantruong020302@gmail.com>
2023-06-26 09:22:31 +0700

    Resolving ambiguous actions

--- Module3/Controllers/HomeController.cs
+++ Module3/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
 
 namespace Module3.Controllers
 {
@@ -7,6 +8,16 @@ namespace Module3.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public string Edit(int id)
+        {
+            return "Hello from Edit with Id = " + id;
+        }
+        [HttpPost]
+        public string Edit(int id, string s)
+        {
+            return "Hello from Edit POST method with Id = " + id;
         }
     }
 }
